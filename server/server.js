32aed0cc45837008fd4e8c1dce0e142d2ea7b4e8
Yuneysa S <ysuarez@20h.es>
2018-05-06 18:34:24 +0100

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const path = require('path');   //no necesita ser instalado, tiene un constructor en el módulo.
+// http -> librería que se usa para crear un servidor.
+const http = require('http');   //no necesita ser instalado. Lo usa express behind the scenes.
 const express = require('express');
+// sockect.io -->  tiene libreria de front end y de back end
+const socketIO = require('socket.io');  //librería que permite la comunicacion entre cliente y servidor y viceversa.
 
 const publicPath = path.join(__dirname, '../public');   //para que anide las rutas de directorios y se guarde la carpeta public en publicPath.
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app); 
+/*en realidad CreateServer recoge una función con un req y res, pero como express 
+lo usa detras de las scenes, se le puse pasar la app(que lo llama).*/
+let io = socketIO(server);  //almacenamos en io el socket del server para luego usarlo.
+
 
 app.use(express.static(publicPath));    //indicamos la ruta estática que vamos a usar y la use.
 
-app.listen(port, () =>{
+// permite registrar un evento de escucha y se puede escuchar por un evento especifico y hacer algo cuando éste ocurra.
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected.');
+    });
+
+});
+
+
+
+/* app.listen(port, () =>{  --> cuando se usa el http(como se almacenó en server) y se crea un servidor, 
+    para luego decirle el puerto por el que escucha, se usa "server" en vez de "app".*/
+server.listen(port, () =>{
     console.log(`Server is up on port ${port}`);
 });
